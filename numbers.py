d2c932ede6eb0d44c527d66547c7ef4cc6ff25a6
deekshithvenkat <deekshit777@gmail.com>
2020-11-13 18:17:24 +0000

    python numbers

---
+++ numbers.py
@@ -0,0 +1,16 @@
+# Addition of numbers
+print(4+2)
+
+# Subtraction of numbers
+print(2-8)
+
+# multiplication of 2 numbers
+print(2*10)
+
+# Division of 2 numbers
+print(13/5)
+
+# Reminder of divison
+print(13%5)
+
+#
